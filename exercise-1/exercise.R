53080cb224f10f71e842838def449eb79fbcb2aa
Lior Levy <lior7212@gmail.com>
2017-01-17 14:39:51 -0800

    add changes to ex1 and ex2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,27 +1,41 @@
+#module 8
 # Exercise 1: Lists
 
 # Create a vector of everything you ate for breakfast
 
+my.breakfast <- c("cereal", "pancakes", "muffin")
 
 # Create a vector of everything you ate for lunch
 
+my.lunch <- c("chicken", "rice", "potatoes")
 
 # Create a list "meals" that has contains your `breakfast` and `lunch`
 
+meals <- list(breakfast = my.breakfast, lunch = my.lunch)
+print(meals)
 
 # Add a "dinner" element to your "meals" list that has what you plan to eat for dinner
+
+meals$dinner <- c("bread", "cheese" , "cake")
 
 
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
 
 
+dinner <- meals$dinner
+
+
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason)
+
+meals[[5]] <- meals[['lunch']]
 
 
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early.meals`
+
+early.meals <- meals[1:2]
 
 
 
@@ -29,8 +43,20 @@
 # Create a list that has the number of items you ate for each meal
 # Hint: use the `lappy()` function
 
+number.of.items <- lapply(meals, length)
+
+print(number.of.items)
 
 # Write a function that adds pizza to every meal
 
+AddPizza <- function(list){
+  add.pizza <- c(list, "pizza")
+  return(add.pizza)
+} 
 
 # Add pizza to every meal!
+
+pizza <- lapply(meals, AddPizza)
+pizza
+
+
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,11 +1,16 @@
+#module 8
 # Exercise 2: `*apply()`
 
 # Create a *list* of 10 random numbers. Use the `runif` function to make a vector of random numbers,
 # then use `as.list()` to convert that to a list.
 
+random.list <- as.list(runif(10, min=0, max=1))
+
 
 # Use `lapply()` to apply the `round()` function to each number, rounding it to the nearest .1
 
+rounded <- lapply(random.list, round, 1)
+print(rounded)
 
 
 ## Bonus
@@ -13,28 +18,42 @@
 # Create a variable 'sentence' that has contains a sentence of text (go for something longish).
 # Make it lowercase
 
+sentence <- "hello how are you today? the weather is pretty cold and cloudy"
 
 # Use the `strsplit()` function to split the sentence into vector of letters.
 # Hint: split on `""` to split on everything
 # Note: this will return a _list_ with 1 element (which is the vector of letters)
 
+list <- strsplit(sentence, split="")
 
 # Extract the vector of letters from the resulting list
 
+vector <- list[[1]]
 
 # Use the `unique()` function to get a vector unique letters
 
+vector.unique <- unique(vector)
 
 # Define a function CountOccurances that takes in two parameters: a letter and a vector letters.
 # The function should return how many times that letter occurs in the vector
 # Hint: use a filter!
 
+CountOccurances <- function(letter, vector){
+  
+  count <- length(vector[vector == letter])
+  return(count)
+  
+}
+
 
 # Call your CountOccurances() function to see how many times the letter 'e' is in your sentence.
+
+CountOccurances("e", vector)
 
 
 # Use `sapply()` to apply your CountOccurances() function to each unique letter in the vector to determine its frequency!
 # Convert the result into a list (using `as.list`).
 
-
+x <- as.list(sapply(vector, CountOccurances, vector.unique))
 # Print the resulting list of frequencies
+print(x)
